1b1e6d9f035860e22450f9d0d08e22fdefefde54
seigtm <seigtm@gmail.com>
2021-09-11 19:02:45 +0300

    conanfile.py simplified

--- conanfile.py
+++ conanfile.py
@@ -12,27 +12,3 @@ class Polynomial(ConanFile):
 
     build_requires = \
         "gtest/[~1.11]"
-
-    scm = {
-        "type": "git",
-        "subfolder": name,
-        "url": "auto",
-        "revision": "auto",
-        "username": "git"
-    }
-
-    def _configure(self, verbose=True):
-        cmake = CMake(self)
-        cmake.verbose = verbose
-        cmake.definitions['CMAKE_BUILD_TYPE'] = "Debug" if self.settings.build_type == "Debug" else "Release"
-        cmake.configure(source_folder=self.name)
-        return cmake
-
-    def build(self):
-        cmake = self._configure()
-        cmake.build()
-        cmake.test(output_on_failure=True)
-
-    def package(self):
-        cmake = self._configure()
-        cmake.install()
